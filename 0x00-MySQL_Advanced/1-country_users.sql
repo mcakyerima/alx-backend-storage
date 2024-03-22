c5d22f77c98a83b7cc8e6f8327475a06fef46142
mcakyerima <mcakyerima@gmail.com>
2024-03-22 03:42:46 +0100

    ✅ Finished task 2

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Script to create a users table with specified attributes
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US';
+);

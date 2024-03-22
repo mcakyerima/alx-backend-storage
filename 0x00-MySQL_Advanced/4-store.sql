f3a31a228e994e2cef213ab0169fb858ae1d11f2
mcakyerima <mcakyerima@gmail.com>
2024-03-22 04:15:55 +0100

    ✅ Finished task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- Create trigger to decrease quantity after adding a new order
+DELIMITER //
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END//
+DELIMITER ;

baf279fac9d55afea3ecaf8398f945b8e1857f46
mcakyerima <mcakyerima@gmail.com>
2024-03-22 03:47:49 +0100

    ✅ Finished task 2

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US';
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
